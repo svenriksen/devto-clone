42e6c6b04b29aa49c9c8d2b7e38239ca4bb7002c
Tran Nguyen Sinh <kaisvenriksen@gmail.com>
2024-10-22 22:46:27 +1100

    fix ui

--- src/app/[userId]/[postId]/page.tsx
+++ src/app/[userId]/[postId]/page.tsx
@@ -73,8 +73,8 @@ export default function Post({ params }: { params: { userId: string, postId: str
             <div className="max-w-[1308px] w-full mx-auto grid grid-cols-1 gap-0 md:gap-2 md:p-2 md:grid-cols-[4rem_1fr] lg:grid-cols-[4rem_7fr_3fr] lg:gap-4 lg:p-4">
                 <aside className="hidden md:grid gap-6 justify-center items-start mt-[calc(56px+6vh)] sticky w-full top-[calc(56px+6vh)] ">
                     <div className="grid gap-6 items-center">
-                        <button onClick={(event) => reactToPost(event)}>
-                            <svg className={(react ? "!fill-red-500" : "") + " hover:bg-black/20 rounded-lg"} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#eb4d05" role="img" aria-hidden="true" >
+                        <button onClick={(event) => reactToPost(event)} className="hover:!fill-[#eb4d05]">
+                            <svg className={(react ? "fill-[#eb4d05]" : "") + " hover:!fill-[#eb4d05] rounded-lg"} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="img" aria-hidden="true" >
                                 <g clipPath="url(#clip0_988_3276)">
                                     <path d="M19 14V17H22V19H18.999L19 22H17L16.999 19H14V17H17V14H19ZM20.243 4.75698C22.505 7.02498 22.583 10.637 20.479 12.992L19.059 11.574C20.39 10.05 20.32 7.65998 18.827 6.16998C17.324 4.67098 14.907 4.60698 13.337 6.01698L12.002 7.21498L10.666 6.01798C9.09103 4.60598 6.67503 4.66798 5.17203 6.17198C3.68203 7.66198 3.60703 10.047 4.98003 11.623L13.412 20.069L12 21.485L3.52003 12.993C1.41603 10.637 1.49503 7.01898 3.75603 4.75698C6.02103 2.49298 9.64403 2.41698 12 4.52898C14.349 2.41998 17.979 2.48998 20.242 4.75698H20.243Z" fill="#525252"></path>
                                 </g>
@@ -88,8 +88,8 @@ export default function Post({ params }: { params: { userId: string, postId: str
                             {reactionsCount}
                         </button>
 
-                        <button className="hover:bg-black/20 rounded-lg">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="img" aria-hidden="true">
+                        <button className="rounded-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="hover:fill-orange-500" role="img" aria-hidden="true">
                                 <path d="M10 3h4a8 8 0 010 16v3.5c-5-2-12-5-12-11.5a8 8 0 018-8zm2 14h2a6 6 0 000-12h-4a6 6 0 00-6 6c0 3.61 2.462 5.966 8 8.48V17z"></path>
                             </svg>
                             {comments?.length}
@@ -102,7 +102,7 @@ export default function Post({ params }: { params: { userId: string, postId: str
                     </div>
                 </aside >
                 <div className="bg-white rounded-lg pb-8">
-                    {(post.data?.coverImage !== null && post.data?.coverImage.replace("data:application/octet-stream;base64,", "").trim() !== "") ? <Image alt="" src={post.data?.coverImage ?? ""} width={2000} height={2000} />
+                    {(post.data?.coverImage !== null && post.data?.coverImage.replace("data:application/octet-stream;base64,", "").trim() !== "") ? <Image alt="" src={post.data?.coverImage ?? ""} style={{ borderTopLeftRadius: "0.375rem", borderTopRightRadius: "0.375rem" }} width={2000} height={2000} />
                         : null}
                     <div className="pt-8 px-12 relative md:px-16">
 
@@ -178,6 +178,7 @@ export default function Post({ params }: { params: { userId: string, postId: str
                                     </div>
                                 </div>
                                 <div className="p-4">
+
 
                                     <button className="bg-[rgb(59,73,223)] w-full font-medium text-white hover:bg-[rgb(47,58,178)] rounded-lg py-2 mb-4">
                                         Follow
