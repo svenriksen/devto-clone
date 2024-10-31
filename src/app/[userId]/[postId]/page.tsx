0968d736721ddd036742f415c5ed3c2f2099657c
Tran Nguyen Sinh <kaisvenriksen@gmail.com>
2024-10-31 14:19:21 +1100

    added comment section

--- src/app/[userId]/[postId]/page.tsx
+++ src/app/[userId]/[postId]/page.tsx
@@ -132,12 +132,14 @@ function Post({ params }: { params: { userId: string, postId: string } }) {
                             </button>
                         </Tooltip>
                         <Tooltip className="!w-fit bg-black/90 text-white text-sm py-1 px-2 rounded-lg" placement="bottom" size="sm" content="Jump to comment">
-                            <button className="rounded-lg">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="hover:fill-orange-500" role="img" aria-hidden="true">
-                                    <path d="M10 3h4a8 8 0 010 16v3.5c-5-2-12-5-12-11.5a8 8 0 018-8zm2 14h2a6 6 0 000-12h-4a6 6 0 00-6 6c0 3.61 2.462 5.966 8 8.48V17z"></path>
-                                </svg>
-                                {comments?.length}
-                            </button>
+                            <Link href={"#comment-section"}>
+                                <button className="rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="hover:fill-orange-500" role="img" aria-hidden="true">
+                                        <path d="M10 3h4a8 8 0 010 16v3.5c-5-2-12-5-12-11.5a8 8 0 018-8zm2 14h2a6 6 0 000-12h-4a6 6 0 00-6 6c0 3.61 2.462 5.966 8 8.48V17z"></path>
+                                    </svg>
+                                    {comments?.length}
+                                </button>
+                            </Link>
                         </Tooltip>
                         <Tooltip className="!w-fit bg-black/90 text-white text-sm py-1 px-2 rounded-lg" placement="bottom" size="sm" content="Save">
                             <button>
